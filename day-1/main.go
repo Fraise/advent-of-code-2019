6d13b08e99930a8e86c5d0c367968112f5ab4439
Alex J <Fraise@users.noreply.github.com>
2019-12-02 10:10:05 -0500

    Added day 1 part 2.

--- day-1/main.go
+++ day-1/main.go
@@ -9,8 +9,16 @@ import (
 )
 
 func main() {
+
+	sum1 := part1(os.Args[1])
+	sum2 := part2(os.Args[1])
+
+	fmt.Println("Sum1 = " + strconv.Itoa(sum1))
+	fmt.Println("Sum2 = " + strconv.Itoa(sum2))
+}
+
+func part1(inputFile string) (sum int) {
 	var moduleList []int
-	var sum int = 0
 
 	f, err := os.Open(os.Args[1])
 	defer f.Close()
@@ -34,5 +42,41 @@ func main() {
 		sum += m/3 - 2
 	}
 
-	fmt.Println("Sum = " + strconv.Itoa(sum))
+	return sum
+}
+
+func part2(inputFile string) (sum int) {
+	var moduleList []int
+
+	f, err := os.Open(inputFile)
+	defer f.Close()
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	s := bufio.NewScanner(f)
+	for s.Scan() {
+		m, err := strconv.Atoi(s.Text())
+
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		moduleList = append(moduleList, m)
+	}
+
+	for _, m := range moduleList {
+		modFuel := m/3 - 2
+		sum += modFuel
+
+		fuel := modFuel/3 - 2
+
+		for fuel > 0 {
+			sum += fuel
+			fuel = fuel/3 - 2
+		}
+	}
+
+	return sum
 }
